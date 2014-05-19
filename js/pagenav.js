435620de737de06174990e9700cfd3540af1c435
Brian Morris <bkmorris8@gmail.com>
2014-05-18 20:17:52 -0400

    added pagnav.js, added page section divs, minor style changes

---
+++ js/pagenav.js
@@ -0,0 +1,135 @@
+// Home
+$('#home-nav').click(function() {
+	$('#personal-left').css('display', '');
+    $('#about-left').css('display', '');
+    $('#location-left').css('display', '');
+    $('#contact-left').css('display', '');
+    $('#privacy-left').css('display', '');
+    $('#disclaimer-left').css('display', '');
+    $('#sitemap-left').css('display', '');
+    $('#accessibility-left').css('display', '');
+
+});
+
+
+// About
+$('#about-nav').click(function() {
+	$('#personal-left').css('display', 'none');
+    $('#about-left').css('display', 'block');
+    $('#location-left').css('display', '');
+    $('#contact-left').css('display', '');
+    $('#privacy-left').css('display', '');
+    $('#disclaimer-left').css('display', '');
+    $('#sitemap-left').css('display', '');
+    $('#accessibility-left').css('display', '');    
+});
+
+// Location
+$('#location-nav').click(function() {
+    $('#personal-left').css('display', 'none');
+    $('#about-left').css('display', '');
+    $('#location-left').css('display', 'block');
+    $('#contact-left').css('display', '');
+    $('#privacy-left').css('display', '');
+    $('#disclaimer-left').css('display', '');
+    $('#sitemap-left').css('display', '');
+    $('#accessibility-left').css('display', '');
+
+});
+// Contact
+$('#contact-nav').click(function() {
+    $('#personal-left').css('display', 'none');
+    $('#about-left').css('display', '');
+    $('#location-left').css('display', '');
+    $('#contact-left').css('display', 'block');
+    $('#privacy-left').css('display', '');
+    $('#disclaimer-left').css('display', '');
+    $('#sitemap-left').css('display', '');
+    $('#accessibility-left').css('display', '');
+
+});
+// Privacy
+$('#privacy-nav').click(function() {
+    $('#personal-left').css('display', 'none');
+    $('#about-left').css('display', '');
+    $('#location-left').css('display', '');
+    $('#contact-left').css('display', '');
+    $('#privacy-left').css('display', 'block');
+    $('#disclaimer-left').css('display', '');
+    $('#sitemap-left').css('display', '');
+    $('#accessibility-left').css('display', '');
+
+});
+// Disclaimer
+$('#disclaimer-nav').click(function() {
+    $('#personal-left').css('display', 'none');
+    $('#about-left').css('display', '');
+    $('#location-left').css('display', '');
+    $('#contact-left').css('display', '');
+    $('#privacy-left').css('display', '');
+    $('#disclaimer-left').css('display', 'block');
+    $('#sitemap-left').css('display', '');
+    $('#accessibility-left').css('display', '');
+
+});
+// Site Map
+$('#sitemap-nav').click(function() {
+    $('#personal-left').css('display', 'none');
+    $('#about-left').css('display', '');
+    $('#location-left').css('display', '');
+    $('#contact-left').css('display', '');
+    $('#privacy-left').css('display', '');
+    $('#disclaimer-left').css('display', '');
+    $('#sitemap-left').css('display', 'block');
+    $('#accessibility-left').css('display', '');
+
+});
+// Accessibility
+$('#accessibility-nav').click(function() {
+    $('#personal-left').css('display', 'none');
+    $('#about-left').css('display', '');
+    $('#location-left').css('display', '');
+    $('#contact-left').css('display', '');
+    $('#privacy-left').css('display', '');
+    $('#disclaimer-left').css('display', '');
+    $('#sitemap-left').css('display', '');
+    $('#accessibility-left').css('display', 'block');
+
+});
+
+
+
+$('#personal').click(function() {
+	$('#personal').css('font-style', '');
+	$('#business').css('font-style', '');
+	$('#option').css('background', ''); 
+	$('#freequote-form').css('background-color', '');
+	$('#freequote-form').css('border', '');
+	$('#testimonials').css('background-color', '');
+	$('#testimonials').css('border', '');
+    $('#personal-brochures').css('display', '');
+    $('#business-brochures').css('display', 'none');
+    $('#resources').css('background-color', '');
+});
+
+$('#business').click(function() {	
+	$('#personal').css('font-style', 'normal');
+	$('#business').css('font-style', 'italic');
+	$('#freequote-form').css('background-color', '#9CCEFF');
+	$('#freequote-form').css('border', '1px solid #499bea');
+	$('#freequote-form').css('border-top', '');
+	$('#testimonials').css('background-color', '#9CCEFF');
+	$('#testimonials').css('border', '1px solid #499bea');
+	$('#testimonials').css('border-top', '');
+	$('#option').css('background', '#499bea');
+	$('#option').css('background', 'url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzQ5OWJlYSIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMyMDdjZTUiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+)');
+	$('#option').css('background', '-moz-linear-gradient(top,  #499bea 0%, #207ce5 100%)');
+	$('#option').css('background', '-webkit-gradient(linear, left top, left bottom, color-stop(0%,#499bea), color-stop(100%,#207ce5))');
+	$('#option').css('background', '-webkit-linear-gradient(top,  #499bea 0%,#207ce5 100%)');
+	$('#option').css('background', '-o-linear-gradient(top,  #499bea 0%,#207ce5 100%)');
+	$('#option').css('background', '-ms-linear-gradient(top,  #499bea 0%,#207ce5 100%)');
+	$('#option').css('background', 'linear-gradient(to bottom,  #499bea 0%,#207ce5 100%)');
+    $('#resources').css('background-color', '#9CCEFF');
+    $('#personal-brochures').css('display', 'none');
+    $('#business-brochures').css('display', 'block');
+});
